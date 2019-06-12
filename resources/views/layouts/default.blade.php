0b79cc372b75539dd5f70b9e94290ad162908e42
Eryelv <95273965@qq.com>
2019-06-12 04:17:10 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title', 'Weibo App') - Laravel</title>
+  </head>
+  <body>
+      @yield('content')
+  </body>
+</html>

817a0390d1337e594d64ea77d05bf3fb5377a452
zihang <95273965@qq.com>
2019-10-30 03:14:28 +0000

    用户激活

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
       public function __construct()
       {
           $this->middleware('auth', [
-              'except' => ['show', 'create', 'store', 'index']
+              'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
           ]);
 
           $this->middleware('guest', [
@@ -49,11 +50,11 @@ class UsersController extends Controller
               'password' => bcrypt($request->password)
           ]);
 
-          Auth::login($user);
+          $this->sendEmailConfirmationTo($user);
 
-          session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+          session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
 
-          return redirect()->route('users.show', [$user]);
+          return redirect('/');
       }
 
       public function edit(User $user)
@@ -102,5 +103,32 @@ class UsersController extends Controller
           $user->delete();
           session()->flash('success', '成功删除用户！');
           return back();
+      }
+
+      public function confirmEmail($token)
+      {
+          $user = User::where('activation_token', $token)->firstOrFail();
+
+          $user->activated = true;
+          $user->activation_token = null;
+          $user->save();
+
+          Auth::login($user);
+          session()->flash('success', '恭喜你，激活成功！');
+          return redirect()->route('users.show', [$user]);
+      }
+
+      protected function sendEmailConfirmationTo($user)
+      {
+          $view = 'emails.confirm';  //邮件要发送的html视图
+          $data = compact('user');  //视图中的参数
+          $from = '95273965@qq.com';  //发送人地址
+          $name = 'Eryelv';  //发送人名称
+          $to = $user->email;  //收件人地址
+          $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";  //邮件主题
+
+          Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+              $message->from($from, $name)->to($to)->subject($subject);
+          });
       }
 }
